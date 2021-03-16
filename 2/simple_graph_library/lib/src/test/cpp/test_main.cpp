1cb56e1bd4befae9ccc015d6cb118e991e9bd841
dazzlemon <daanill13470@gmail.com>
2021-03-16 20:42:30 +0200

    more ascii art

--- 2/simple_graph_library/lib/src/test/cpp/test_main.cpp
+++ 2/simple_graph_library/lib/src/test/cpp/test_main.cpp
@@ -1,7 +1,19 @@
 #include "adjacency_list_graph_test.h"
 #include "adjacency_matrix_graph_test.h"
 
+#include <iostream>
+
 auto main() -> int {
+    auto sgl_ascii = R"(
+   _____  ______ __ 
+  / ___/ / ____// /  
+  \__ \ / / __ / /  
+ ___/ // /_/ // /___
+/____/ \____//_____/
+
+)";// tabs are preserved in raw strings
+    std::cout << sgl_ascii;
+
     adjacency_matrix_graph_tests();
     adjacency_list_graph_tests();
 }
